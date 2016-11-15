6866252e7803b423201b61ef9c3bce7266b35f18
emma <etakuu90@gmail.com>
2016-11-15 10:15:59 -0800

    first circle

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,11 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function makeShape(){
+ var circle = document.createElementNS(namespace, "circle")
+ circle.setAttribute("cx", 100)
+circle.setAttribute("cy", 100)
+circle.setAttribute("r", 40)
+var canvas = document.getElementById("game-board")
+canvas.appendChild(circle)
+}
